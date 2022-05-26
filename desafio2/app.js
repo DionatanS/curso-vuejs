d1baa96da3db9abf2c655800bf948fd2dc5b1180
Dionatan da Silva <dionatan@brdtech.com.br>
2022-05-26 16:05:36 -0300

    Adcionado novo desafio

---
+++ desafio2/app.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        valor: ''
+    },
+	methods: {
+		alerta(){
+			alert('Botão foi clicado!')
+		},
+		// editName(event){
+		// 	this.valor = event.target.value;
+		// }
+	}
+})
